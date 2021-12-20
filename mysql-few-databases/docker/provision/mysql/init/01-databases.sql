40435c9a9592c3979a49c9bb0bf106a29c4eb3d2
songjuchao <songjuchao@gs-robot.com>
2021-12-20 20:56:50 +0800

    feat: add databases

--- mysql-few-databases/docker/provision/mysql/init/01-databases.sql
+++ mysql-few-databases/docker/provision/mysql/init/01-databases.sql
@@ -1,7 +1,10 @@
 # create databases
-CREATE DATABASE IF NOT EXISTS `primary`;
-CREATE DATABASE IF NOT EXISTS `secondary`;
+CREATE DATABASE IF NOT EXISTS `iam`;
+CREATE DATABASE IF NOT EXISTS `gs_auth`;
+CREATE DATABASE IF NOT EXISTS `gs_robot`;
+CREATE DATABASE IF NOT EXISTS `cedar`;
+CREATE DATABASE IF NOT EXISTS `bot`;
 
 # create root user and grant rights
-CREATE USER 'root'@'localhost' IDENTIFIED BY 'local';
+-- CREATE USER 'root'@'localhost' IDENTIFIED BY 'not_a_secret';
 GRANT ALL ON *.* TO 'root'@'%';
